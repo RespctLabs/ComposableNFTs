f9a2116db517c49c502e0f93570e540f325aa292
waverune <yashviknaithani@gmail.com>
2022-03-12 03:23:08 +0530

    init migrations

--- migrations/02_deploy_ERC998ERC1155TopDownPresetMinterPauser.js
+++ migrations/02_deploy_ERC998ERC1155TopDownPresetMinterPauser.js
@@ -2,13 +2,13 @@ const ERC998ERC1155TopDownPresetMinterPauser = artifacts.require("ERC998ERC1155T
 const ERC1155PresetMinterPauser = artifacts.require("ERC1155PresetMinterPauser");
 
 
-let name  = "";
-let symbol = "";
-let baseURI ="";
+let name    = "fun";
+let symbol  = "FUN";
+let baseURI = "fun.com/{id}";
 let csnftPrice = 1000;
 
 // name, string memory symbol, string memory baseURI
 module.exports = function (deployer) {
- deployer.deploy(ERC998ERC1155TopDownPresetMinterPauser, name, symbol, baseURI,csnftPrice);
+  deployer.deploy(ERC998ERC1155TopDownPresetMinterPauser, name, symbol, baseURI,csnftPrice);
   deployer.deploy(ERC1155PresetMinterPauser,"https://ERC1155.com/{id}");
 };
